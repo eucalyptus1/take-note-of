46cf3291c36485b6c2c95c21c3a43496607416de
James Harrington <jamesharrington585@hotmail.com>
2023-01-17 23:35:51 -0500

    not sure if recent changes had anything to do with it, but app now appears on localhost

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require ('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3001;
@@ -7,7 +8,7 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', htmlRoutes);
 

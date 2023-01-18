de48ee18f40440c33b0c33bbf2bb9c32c370c0fb
James Harrington <jamesharrington585@hotmail.com>
2023-01-17 21:02:07 -0500

    still building html routes. slight progress, but not fully connected yet

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const express = require ('express');
 const app = express();
 const PORT = 3001;
 
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use('/', htmlRoutes);
+
 app.listen(PORT, () => {
     console.log(`The app is connected at ${PORT}!`);
   });

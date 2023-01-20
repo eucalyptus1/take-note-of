9308e63cf3d2db57822a4c310481ab2f72065dc9
James Harrington <jamesharrington585@hotmail.com>
2023-01-20 00:48:26 -0500

    deploying to heroku

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const app = express();
 const PORT = 3001;
 
 const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
     console.log(`The app is connected at ${PORT}!`);
